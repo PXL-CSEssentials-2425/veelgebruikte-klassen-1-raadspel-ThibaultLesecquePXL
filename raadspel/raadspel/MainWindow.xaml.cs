7b27ee07220d4aedd5a647ff8453f6bd63979af8
ThibaultLesecquePXL <thibault.lesecque@student.pxl.be>
2024-10-10 13:25:32 +0200

    Versie 0.2 - Functies deel 1

--- raadspel/raadspel/MainWindow.xaml.cs
+++ raadspel/raadspel/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Security.Cryptography;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,31 @@ namespace raadspel
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random rnd = new Random();
+
+        int randomNumber;
+        int pickedNumber;
+
         public MainWindow()
         {
             InitializeComponent();
+            randomNumber = rnd.Next(1, 100);
+        }
+
+        private void evaluateButton_Click(object sender, RoutedEventArgs e)
+        {
+            int.TryParse(numberTextBox.Text, out pickedNumber);
+            
+        }
+
+        private void newButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void endButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
