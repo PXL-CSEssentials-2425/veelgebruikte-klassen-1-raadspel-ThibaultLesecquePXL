2693849f6e0ad63a29ac8d20c89a6beca639f644
ThibaultLesecquePXL <thibault.lesecque@student.pxl.be>
2024-10-10 13:39:54 +0200

    Versie 1.0 - Afgewerkte versie

--- raadspel/raadspel/MainWindow.xaml.cs
+++ raadspel/raadspel/MainWindow.xaml.cs
@@ -12,9 +12,6 @@ using System.Windows.Shapes;
 
 namespace raadspel
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         Random rnd = new Random();
