831225dfc8de444232534d1466cc115b24028a48
ThibaultLesecquePXL <thibault.lesecque@student.pxl.be>
2024-10-10 13:33:19 +0200

    Versie 0.3 - Functies deel 2

--- raadspel/raadspel/MainWindow.xaml.cs
+++ raadspel/raadspel/MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace raadspel
 
         int randomNumber;
         int pickedNumber;
+        int amountTries;
 
         public MainWindow()
         {
@@ -31,12 +32,34 @@ namespace raadspel
         private void evaluateButton_Click(object sender, RoutedEventArgs e)
         {
             int.TryParse(numberTextBox.Text, out pickedNumber);
+
+            amountTries += 1;
+            output2TextBox.Text = $"Aantal keren geraden: {amountTries}";
             
+            if (pickedNumber == randomNumber)
+            {
+                output1TextBox.Text = "Proficiat! U hebt het getal geraden!";
+            }
+            else if (pickedNumber > randomNumber)
+            {
+                output1TextBox.Text = "Raad lager!";
+            }
+            else if (pickedNumber < randomNumber)
+            {
+                output1TextBox.Text = "Raad hoger!";
+            }
         }
 
         private void newButton_Click(object sender, RoutedEventArgs e)
         {
+            pickedNumber = 0;
+            amountTries = 0;
 
+            numberTextBox.Clear();
+            output1TextBox.Clear();
+            output2TextBox.Clear();
+
+            randomNumber = rnd.Next(1, 100);
         }
 
         private void endButton_Click(object sender, RoutedEventArgs e)
